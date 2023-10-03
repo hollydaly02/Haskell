12c3e5890af5616cbb1e144d7620668da636e4bd
Holly Daly <98813546+hollydaly02@users.noreply.github.com>
2023-10-03 11:39:26 +0100

    Exercise 2

---
+++ Exercise02/src/Ex02.hs
@@ -0,0 +1,135 @@
+{- butrfeld Andrew Butterfield -}
+module Ex02 where
+
+name, idno, username :: String
+name      =  "Holly Daly"  -- replace with your name
+idno      =  "20331814"    -- replace with your student id
+username  =  "dalyho"   -- replace with your TCD username
+
+
+declaration -- do not modify this
+ = unlines
+     [ ""
+     , "@@@ This exercise is all my own work."
+     , "@@@ Signed: " ++ name
+     , "@@@ "++idno++" "++username
+     ]
+
+-- Datatypes and key functions -----------------------------------------------
+
+-- do not change anything in this section !
+
+type Id = String
+
+data Expr
+  = Val Double
+  | Add Expr Expr
+  | Mul Expr Expr
+  | Sub Expr Expr
+  | Dvd Expr Expr
+  | Var Id
+  | Def Id Expr Expr
+  deriving (Eq, Show)
+
+type Dict k d  =  [(k,d)]
+
+define :: Dict k d -> k -> d -> Dict k d
+define d s v = (s,v):d
+
+find :: Dict String d -> String -> Either String d
+find []             name              =  Left ("undefined var "++name)
+find ( (s,v) : ds ) name | name == s  =  Right v
+                         | otherwise  =  find ds name
+
+type EDict = Dict String Double
+
+v42 = Val 42 ; j42 = Just v42
+
+-- do not change anything above !
+
+-- Part 1 : Evaluating Expressions -- (50 test marks, worth 25 Exercise Marks) -
+
+-- Implement the following function so all 'eval' tests pass.
+
+-- eval should return `Left msg` if:
+  -- (1) a divide by zero operation was going to be performed;
+  -- (2) the expression contains a variable not in the dictionary.
+  -- see test outcomes for the precise format of those messages
+
+eval :: EDict -> Expr -> Either String Double
+--eval d e = error "eval NYI"
+eval d (Var x) = find d x
+eval _ (Val x) = Right x
+
+eval d (Add x y) = case (eval d x,eval d y) of
+    (Right m, Right n) -> Right(m + n)
+    (Left m, _)        -> Left m
+    (_, Left n)        -> Left n
+    
+eval d (Sub x y) = case (eval d x,eval d y) of
+    (Right m, Right n) -> Right(m - n)
+    (Left m, _)        -> Left m
+    (_, Left n)        -> Left n
+
+eval d (Mul x y) = case (eval d x,eval d y) of
+    (Right m, Right n) -> Right(m * n)
+    (Left m, _)        -> Left m
+    (_, Left n)        -> Left n
+
+eval d (Dvd x y) = case (eval d x,eval d y) of
+    (Right m, Right 0) -> Left "div by zero"
+    (Right 0, Right n) -> Left "div by zero"
+    (Right m, Right n) -> Right(m / n)
+    (Left m, _)        -> Left m
+    (_, Left n)        -> Left n
+
+eval d (Def a x y) = let z = eval d x in case (z) of 
+    (Right z)        -> eval (define d a z) y
+    _                -> Left "div by zero"
+
+-- Part 2 : Expression Laws -- (15 test marks, worth 15 Exercise Marks) --------
+
+{-
+
+There are many, many laws of algebra that apply to our expressions, e.g.,
+
+  x * y            =   y * z        Law 1
+  (x + y) + z      =   x + (y + z)  Law 2
+  (x / y) / z      =   x / (y * z)  Law 3
+  (x + y)*(x - y)  =  x*x - y*y     Law 4
+  ...
+
+  We can implement these directly in Haskell using Expr
+
+  Function LawN takes an expression:
+    If it matches the "shape" of the law lefthand-side,
+    it replaces it with the corresponding righthand "shape".
+    If it does not match, it returns Nothing
+
+    Implement Laws 1 through 4 above
+-}
+
+-- x * y            =   y * z        Law 1
+law1 :: Expr -> Maybe Expr
+-- law1 e = error "law1 NYI"
+law1 (Mul x y) = Just (Mul y x)
+law1 e = Nothing
+
+-- (x + y) + z      =   x + (y + z)  Law 2
+law2 :: Expr -> Maybe Expr
+--law2 e = error "law2 NYI"
+law2 (Add (Add x y) z) = Just (Add x (Add y z))
+law2 e = Nothing
+
+-- (x / y) / z      =   x / (y * z)  Law 3
+law3 :: Expr -> Maybe Expr
+--law3 e = error "law3 NYI"
+law3 (Dvd (Dvd x y) z) = Just (Dvd x (Mul y z))
+law3 e = Nothing
+
+-- (x + y)*(x - y)  =  x*x - y*y     Law 4
+law4 :: Expr -> Maybe Expr
+--law4 e = error "law4 NYI"
+law4 (Mul (Add x y) (Sub m n)) = Just (Sub (Mul x m) (Mul y n))
+law4 e = Nothing
+
